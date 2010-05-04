c7ebaf47a54dd544404250de5ed13119c1272d42
Drahkir <rikmikac@gmail.com>
2010-05-04 03:54:09 -0400

    Functional JApplet

--- src/othello/Main.java
+++ src/othello/Main.java
@@ -15,33 +15,39 @@ import java.awt.event.ActionListener;
  */
 
 public class Main extends JApplet implements ActionListener {
+
+    public Main() {
+        //empty constructor
+    }
+    public void init() {
+        Board board = new Board();
+        
+        JFrame frame = new JFrame();
+        frame.add(board);
+        int frame_height, frame_width;
+        frame_height = frame.getHeight();
+        frame_width = frame.getWidth();
+        frame.setSize((board.tile_height * board.board_row) + 3, (board.tile_width * board.board_col) + 33);
+        MyMouseHandler mh = new MyMouseHandler();
+        frame.addMouseListener(mh);
+        frame.setVisible(true);
+
+
+        Game game = new Game(board, mh, true, false); //true for human player, false for CPU
+        game.PlayGame();
+    }
     public void actionPerformed(ActionEvent e) {
         //Necessary for abstract class
     }
  
     public static void main(String[] args) {
-        Board board = new Board(); //uncomment for application version
-        //board.init(); //uncomment for applet version
+        //Board board = new Board(); //uncomment for application version
+        Main m = new Main();
+        m.init(); //uncomment for applet version
 
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize((board.tile_height * board.board_row) + 3, (board.tile_width * board.board_col) + 33);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-        frame.add(board); //Uncomment for application version
-        int frame_height, frame_width;
-        frame_height = frame.getHeight();
-        frame_width = frame.getWidth();
-        MyMouseHandler mh = new MyMouseHandler();
-        frame.addMouseListener(mh);
-
-        Game game = new Game(board, mh, true, false); //true for human player, false for CPU
-        game.PlayGame();
-        System.out.println("Game over");
-        String string = board.declareWinner();
-        System.out.println(string);
-        JLabel winner = new JLabel(string);
-        frame.add(winner);
-        frame.repaint();
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.setLocationRelativeTo(null);
+        //frame.add(board); //Uncomment for application version
+        
     }
 }
